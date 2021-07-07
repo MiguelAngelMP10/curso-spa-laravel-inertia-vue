722417ca7c7291af3a55be99e3da4a317c142fc8
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-07-06 23:39:03 -0500

    Buscador

--- resources/js/Pages/Notes/Index.vue
+++ resources/js/Pages/Notes/Index.vue
@@ -20,23 +20,33 @@
           </div>
           <div class="md:col-span-2 mt-5 md:mt-0">
             <div class="shadow bg-white md:rounded-md">
-              <inertia-link
-                :href="route('notes.create')"
-                class="
-                  bg-transparent
-                  hover:bg-green-500
-                  text-green-700
-                  font-semibold
-                  hover:text-white
-                  py-2
-                  px-4
-                  border border-green-500
-                  hover:border-transparent
-                  rounded
-                "
-              >
-                Crear
-              </inertia-link>
+              <div class="flex justify-between">
+                <input
+                  type="text"
+                  class="form-input rounded-md shadow-sm"
+                  placeholder="Buscar..."
+                  v-model="q"
+                />
+
+                <inertia-link
+                  :href="route('notes.create')"
+                  class="
+                    bg-transparent
+                    hover:bg-green-500
+                    text-green-700
+                    font-semibold
+                    hover:text-white
+                    py-2
+                    px-4
+                    border border-green-500
+                    hover:border-transparent
+                    rounded
+                  "
+                >
+                  Crear
+                </inertia-link>
+              </div>
+
               <table class="pt-5">
                 <tr v-for="(note, i) in notes" :key="i">
                   <td class="border px-4 py-2">
@@ -98,6 +108,16 @@ export default {
   },
   props: {
     notes: Array,
+  },
+  data() {
+    return {
+      q: "",
+    };
+  },
+  watch: {
+    q: function (value) {
+      this.$inertia.replace(this.route("notes.index", { q: value }));
+    },
   },
 };
 </script>
